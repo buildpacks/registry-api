160615009058a6b9e9bea1a753b38901dbe3d748
Joe Kutner <jpkutner@gmail.com>
2021-01-25 00:16:26 -0600

    Added a basic detail page (#6)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,18 @@
 class HomepageController < ApplicationController
+  before_action :set_buildpack, only: [:show]
+
   def index
+  end
+
+  def show
+    if @buildpack
+      render
+    else
+      head 404
+    end
+  end
+
+  private def set_buildpack
+    @buildpack = Buildpack.where(namespace: params[:namespace], name: params[:name]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'homepage#index'
+
+  namespace :buildpacks do
+    get ':namespace/:name', to: '/homepage#show'
+    get ':namespace/:name/version/:version', to: '/homepage#show'
+  end
+
   namespace :api do
     namespace :v1 do #, constraints: ApiConstraint.new(version: "1.buildpack-registry") do
       get 'search', to: 'search#index'
