2386df1148b5afd8f64a2a1d2b42ec4cafa08e3a
Travis <longoria.public@gmail.com>
2021-02-03 10:08:40 -0700

    fix: recalculate urls on every page visit
    Signed-off-by: Travis <longoria.public@gmail.com>

--- app/javascript/components/common/Header.tsx
+++ app/javascript/components/common/Header.tsx
@@ -4,24 +4,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import './Header.scss';
 import { Container } from 'react-bootstrap';
 
-const Header: React.FC = () => {
-    let hostURL = process.env.APPLICATION_HOST || 'http://localhost:3000';
-    if (process.env.HEROKU_APP_NAME) {
-        hostURL = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
-    }
+class Header extends React.Component<any, any> {
+    render() {
+        let hostURL = process.env.APPLICATION_HOST || 'http://localhost:3000';
+        if (process.env.HEROKU_APP_NAME) {
+            hostURL = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
+        }
 
-    return (
-        <header className="Header">
-            <Container>
-                <Navbar expand="sm" sticky="top">
-                    <Navbar.Brand>
-                        <a href={hostURL}><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                </Navbar>
-            </Container>
-        </header>
-    );
+        return (
+            <header className="Header">
+                <Container>
+                    <Navbar expand="sm" sticky="top">
+                        <Navbar.Brand>
+                            <a href={hostURL}><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    </Navbar>
+                </Container>
+            </header>
+        );
+    }
 }
 
 export default Header;
