a9288b8de3eb84a1b268261c1e1ed9eaa94f940d
Arnav Tiwari <avznav@gmail.com>
2021-02-12 11:41:56 +0530

    Made logo link relative (#26)
    Signed-off-by: arnav-t <avznav@gmail.com>

--- app/javascript/components/common/Header.tsx
+++ app/javascript/components/common/Header.tsx
@@ -6,17 +6,12 @@ import { Container } from 'react-bootstrap';
 
 class Header extends React.Component<any, any> {
     render() {
-        let hostURL = process.env.APPLICATION_HOST || 'http://localhost:3000';
-        if (process.env.HEROKU_APP_NAME) {
-            hostURL = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
-        }
-
         return (
             <header className="Header">
                 <Container>
                     <Navbar expand="sm" sticky="top">
                         <Navbar.Brand>
-                            <a href={hostURL}><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
+                            <a href="/"><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     </Navbar>
