6f0e57b8b2692e09ec5d765b39016d9609720ddf
Travis <longoria.public@gmail.com>
2021-02-16 17:46:45 -0700

    spread header contents out
    Signed-off-by: Travis <longoria.public@gmail.com>

--- app/javascript/components/common/Header.tsx
+++ app/javascript/components/common/Header.tsx
@@ -3,26 +3,26 @@ import logo from './logo-bug.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './Header.scss';
-import { Container } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 class Header extends React.Component<any, any> {
     render() {
         return (
             <header className="Header">
-                <Container>
-                    <Navbar expand="sm" sticky="top">
-                        <Navbar.Brand>
-                            <a href="/"><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse>
-                            <Nav className="ml-auto">
-                                <Nav.Link href="https://buildpacks.io/" rel="nofollow noopener noreferrer" target="_blank">Home</Nav.Link>
-                                <Nav.Link href="https://buildpacks.io/docs/buildpack-author-guide/publish-a-buildpack/" rel="nofollow noopener noreferrer" target="_blank">Docs</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </Container>
+                <Navbar expand="sm" sticky="top">
+                    <Navbar.Brand>
+                        <a href="/"><img src={logo} alt="Buildpacks"/></a><span>Registry</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="https://buildpacks.io/" rel="nofollow noopener noreferrer"
+                                      target="_blank">Home</Nav.Link>
+                            <Nav.Link href="https://buildpacks.io/docs/buildpack-author-guide/publish-a-buildpack/"
+                                      rel="nofollow noopener noreferrer" target="_blank">Docs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         );
     }
