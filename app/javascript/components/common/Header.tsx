9b1762f9a39e7638995a899e8c4b872f127b249d
Arnav Tiwari <avznav@gmail.com>
2021-02-17 06:13:42 +0530

    Add links to Home and Docs (#34)
    Signed-off-by: arnav-t <avznav@gmail.com>

--- app/javascript/components/common/Header.tsx
+++ app/javascript/components/common/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo-bug.svg';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './Header.scss';
 import { Container } from 'react-bootstrap';
 
@@ -14,6 +15,12 @@ class Header extends React.Component<any, any> {
                             <a href="/"><img src={logo} alt="Buildpacks" /></a><span>Registry</span>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse>
+                            <Nav className="ml-auto">
+                                <Nav.Link href="https://buildpacks.io/" rel="nofollow noopener noreferrer" target="_blank">Home</Nav.Link>
+                                <Nav.Link href="https://buildpacks.io/docs/buildpack-author-guide/publish-a-buildpack/" rel="nofollow noopener noreferrer" target="_blank">Docs</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Navbar>
                 </Container>
             </header>
