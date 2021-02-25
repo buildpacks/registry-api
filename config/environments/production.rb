28c35d86801d41feeab90452e037666e8b06f5ef
Joe Kutner <jpkutner@gmail.com>
2021-02-25 16:34:32 -0600

    Force https in prod (#45)
    Signed-off-by: Joe Kutner <jpkutner@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = {
+    hsts: {
+      preload: true,
+      expires: 1.year # the default is 180.days
+    }
+  }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
