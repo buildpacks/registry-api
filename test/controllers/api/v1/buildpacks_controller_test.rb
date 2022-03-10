36d6e6ddc8abcc7194790cb6029c57a8bf74c5b5
Ryan Moran <rmoran@vmware.com>
2022-03-10 10:23:28 -0800

    Cast buildpack semver parts to integer when sorting
    Signed-off-by: Ryan Moran <rmoran@vmware.com>

--- test/controllers/api/v1/buildpacks_controller_test.rb
+++ test/controllers/api/v1/buildpacks_controller_test.rb
@@ -8,15 +8,15 @@ class Api::V1::BuildpacksControllerTest < ActionDispatch::IntegrationTest
     assert_equal 2, b.size
     assert_equal 'example', b['latest']['namespace']
     assert_equal 'first', b['latest']['name']
-    assert_equal '1.0.0', b['latest']['version']
+    assert_equal '1.10.0', b['latest']['version']
   end
 
   test "should get show" do
-    get "/api/v1/buildpacks/example/first/1.0.0"
+    get "/api/v1/buildpacks/example/first/1.9.0"
     assert_response :success
     b = JSON.parse(response.body)
     assert_equal 'example', b['namespace']
     assert_equal 'first', b['name']
-    assert_equal '1.0.0', b['version']
+    assert_equal '1.9.0', b['version']
   end
 end
--- test/models/buildpack_test.rb
+++ test/models/buildpack_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class BuildpackTest < ActiveSupport::TestCase
   test "search" do
     b = Buildpack.search("example")
-    assert_equal 2, b.size
+    assert_equal 3, b.size
     assert_equal 'example', b[0].namespace
   end
 end
